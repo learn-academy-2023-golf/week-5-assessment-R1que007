18fe14be661e4fb366e1dcc66e9931e3c083efad
Rique <aguirree1985@gmail.com>
2023-11-06 07:30:18 -0800

    Completed Week 5 assessment

--- interview-questions.md
+++ interview-questions.md
@@ -8,32 +8,38 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. Compare and contrast arrays and hashes in Ruby. Include at least two differences and two similarities.
 
-Your answer:
+Your answer: Arrays and Hashes are similar in the way they store information or data. Hashes, however, use symbols to access that data whereas data in arrays are accessed by calling on the index.
 
 Researched answer:
 
 2. What does it mean to implement a TTD workflow?
 
-Your answer:
+Your answer: A TTD workflow consists of the following:
+-creating a test
+-running a test expecting it to fail 
+-writing code to make the test pass
+-refactoring
+-Repeat
 
-Researched answer:
+
+Researched answer: It means implementing an approach that emphasizes writting tests before writing the actual code.TTD follows a cycle known as "Red-Green-Refator" cycle. Initially, tests should fail for not having implemeted the functionality or written the code. We then get the tests to pass by writting the minimum amount of code possible. Once the code passes we refactor.
 
 3. Why would super be used in a Ruby class?
 
-Your answer:
+Your answer: Super is used to access data in a parent class.
 
-Researched answer:
+Researched answer: In method overriding, super is a keyword used to call a method of the same name from its superclass. It is used to extend or override the behavior of a method that is defined in a parent clas(superclass) while still retaining some or all of the functionality from the parent class. In Construsctor overriding, super is used when you want to modify the constructor of a subclass. The keyword super is used to invoke the constructor of the parent class to ensure that any necessay initialization form the parent class is performed before adding specific initialization for the subclass.
 
 4. STRETCH: What is the difference between inheritance in JavaScript and inheritance in Ruby?
 
-Researched answer:
+Researched answer: Both are ways to create a relationship between classes, however, JS uses prototypal inheritance and Ruby uses classical inheritance with classess inheriting from other classes. Ruby supports multiple inheritance and JavaScript does not.
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. PostgreSQL:
+1. PostgreSQL: Is an open-source relational database management system(RDBMS) used for storing and managing structure data.
 
-2. Ruby on Rails:
+2. Ruby on Rails: Is an open-source web application framework designed to streamline and simplify web application development.
 
-3. Active Record:
+3. Active Record: Is a component of Ruby on Rails that provides an Object Relational Mapping (ORM) layer, allowing Ruby apps to interact with databases using Ruby-based operations.
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -8,12 +8,50 @@ us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['Califor
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Rhode Island', 'Washington'] 
 
 # Pseudo code:
+#Define a method sort_states
+#Extract the values of the array onto a single array
+    #Use .flatten
+#sort the array
+    #Use .sort
+#return the sorted array
+
+def sort_states(hash)
+    combined_sorted_array = hash.values.flatten.sort
+end
+
+result = sort_states(us_states)
+p result
+
 
 # --------------------2a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.
 
 # Pseudo code:
+#Create a class called Bike
+#initialize model, wheels, and current_speed
+#set default wheels = 2
+#set default current speed = 0
+#Create bike_info method 
+    #return a string with all data.
+        #create an instance of Bike, my_bike
+        #call bike_info on my_bike
+
+# class Bike
+#  def initialize (model, wheels = 2, current_speed = 0)
+#     @model = model
+#     @wheels = wheels
+#     @current_speed = current_speed
+#  end
+
+#  def bike_info
+#     "The #{@model} bike has #{@wheels} wheels and is going #{@current_speed} mph"
+#  end
+# end
+# my_bike = Bike.new("Trek") #creates instance of Bike class.
+
+# puts my_bike.bike_info #call the bike_info method on my_bike
+
 
 # -------------------2b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
 
@@ -23,3 +61,49 @@ us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['Califor
 # Expected output example: my_bike.brake(25) => 0
 
 # Pseudo code:
+# Create pedal method pedal_faster with parameter increased_speed
+    #increase @current_speed by increse_speed
+# Create breake method with parameter decrease_speed
+    #decrease @current_speed by decrease_speed
+    # add conditional to output zero if current speed goes below 0
+
+
+class Bike
+    def initialize (model, wheels = 2, current_speed = 0)
+       @model = model
+       @wheels = wheels
+       @current_speed = current_speed
+    end
+   
+    def bike_info
+       "The #{@model} bike has #{@wheels} wheels and is going #{@current_speed} mph"
+    end
+
+    def pedal_faster(increase_speed)
+        @current_speed += increase_speed
+    end
+    
+    def break(decrease_speed)
+        if @current_speed - decrease_speed >= 0
+            @current_speed -= decrease_speed
+        else 
+            @current_speed = 0
+        end
+    end
+end
+
+my_bike = Bike.new("Trek") #creates instance of Bike class.
+puts my_bike.bike_info #call the bike_info method on my_bike
+my_bike.pedal_faster(10) #call pedal_faster method w/ argument (10)
+puts my_bike.bike_info 
+my_bike.pedal_faster(18) #call pedal_faster method w/ argument (18)
+puts my_bike.bike_info
+my_bike.break(5) 
+puts my_bike.bike_info 
+my_bike.break(25)
+puts my_bike.bike_info
+# The Trek bike has 2 wheels and is going 0 mph
+# The Trek bike has 2 wheels and is going 10 mph
+# The Trek bike has 2 wheels and is going 28 mph
+# The Trek bike has 2 wheels and is going 23 mph
+# The Trek bike has 2 wheels and is going 0 mph
